7873b14f2a4a1abacd83cb815a48a1411c731313
Victor Wekesa <wekae.dev@gmail.com>
2021-09-20 02:36:10 +0300

    Challenge 1 - Plane Programming
    Done in Unity 2020.3
    Gameplay video contained in Recordings folder

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(26.0f, 0.37f, 1.1f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Upldate is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
